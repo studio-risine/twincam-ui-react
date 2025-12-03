a58955b7ddc4aa32c883cac28a6715f110e3e80e
Gabriel Melo <gblsmlo@gmail.com>
2025-12-03 11:14:09 -0300

    chore: update stories glob to recursively scan `src/components`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,14 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
 	stories: [
-		'../src/stories/*.stories.@(js|jsx|mjs|ts|tsx)',
+		'../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
 	],
 	addons: [
-        '@storybook/addon-themes',
-        '@storybook/addon-docs',
-        '@storybook/addon-vitest',
-        '@storybook/addon-a11y'
-    ],
+		'@storybook/addon-themes',
+		'@storybook/addon-docs',
+		'@storybook/addon-vitest',
+		'@storybook/addon-a11y'
+	],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {},
